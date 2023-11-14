8ec9d231fea50f41f4a27159353e55fde102eaa9
Germiprogramer <llorentemunozgerman@gmail.com>
2023-11-14 13:46:56 +0100

    commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8$b=xeq5((2b5u5o*a9o#14(v--%q^tpi_7=iyrd%jq+nqo080'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
